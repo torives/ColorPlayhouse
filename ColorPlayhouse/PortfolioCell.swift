937d9a99855854a16364755972da1ae2474202d0
priscilarosa <priscilarosa@MacBook-Pro-de-Priscila.local>
2016-11-18 18:22:09 -0200

    Portfolio VC Collection View

---
+++ ColorPlayhouse/PortfolioCell.swift
@@ -0,0 +1,16 @@
+//
+//  PortfolioCell.swift
+//  ColorPlayhouse
+//
+//  Created by Priscila Rosa on 11/18/16.
+//  Copyright © 2016 Aleixo Rosa & Crispim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PortfolioCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+}
--- ColorPlayhouse/PortfolioViewController.swift
+++ ColorPlayhouse/PortfolioViewController.swift
@@ -8,10 +8,31 @@
 
 import UIKit
 
-class PortfolioViewController: UIViewController
+class PortfolioViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
 {
     var delegate: PortfolioDelegate?
+    //var savedArts: [UIImage] = []
+    var savedArts = [UIImage(named: "desenho1"), UIImage(named: "desenho2"), UIImage(named: "desenho3"), UIImage(named: "desenho4"), UIImage(named: "desenho5")]
     
+    @IBOutlet weak var portfolioCollection: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        portfolioCollection.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return savedArts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "portfolioCell", for: indexPath) as! PortfolioCell
+        
+        cell.image.image = self.savedArts[indexPath.row]
+        
+        return cell
+    }
 }
 
 extension PortfolioViewController: PortfolioScreen

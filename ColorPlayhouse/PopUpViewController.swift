8cc8b1cd5ee0a02119d699e482c0500215e2db34
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2016-11-21 17:20:49 -0200

    Fixed menu button issue

--- ColorPlayhouse/PopUpViewController.swift
+++ ColorPlayhouse/PopUpViewController.swift
@@ -16,6 +16,12 @@ class PopUpViewController: UIViewController {
     
     @IBOutlet weak var screenshotImageView: UIImageView!
     
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var clearAllButton: UIButton!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
     let DAO = DataAccessObject.sharedInstance
     
     var timeLapseBuilder: TimeLapseBuilder?
@@ -28,10 +34,13 @@ class PopUpViewController: UIViewController {
         setImage()
         setupView()
         showAnimation()
+        addMenuButtonRecognizer()
+        setNeedsFocusUpdate()
         
         // Do any additional setup after loading the view.
     }
     
+
     func setImage() {
         let nsDirectory = FileManager.SearchPathDirectory.cachesDirectory
         let nsUserDomainMask = FileManager.SearchPathDomainMask.userDomainMask
@@ -95,6 +104,89 @@ class PopUpViewController: UIViewController {
     }
     
     
+}
+
+extension PopUpViewController {
+    func addMenuButtonRecognizer() {
+        let menuTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DrawingViewController.handlePressMenuButton))
+        menuTapGestureRecognizer.allowedPressTypes = [NSNumber(integerLiteral: UIPressType.menu.rawValue)]
+        self.view.addGestureRecognizer(menuTapGestureRecognizer)
+    }
+    
+    
+    func handlePressMenuButton() {
+        removeAnimation()
+        
+    }
+    
+    override var preferredFocusEnvironments: [UIFocusEnvironment] {
+        return [playButton, menuButton, clearAllButton, saveButton, shareButton]
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        super.didUpdateFocus(in: context, with: coordinator)
+        
+        guard let nextFocusedView = context.nextFocusedView else { return }
+        
+        switch nextFocusedView {
+            
+        case playButton:
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.customFocus(previouslyFocused: self.shareButton, nextFocused: self.playButton, context: context)
+                
+                }, completion: nil)
+            
+        case menuButton:
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.customFocus(previouslyFocused: self.playButton, nextFocused: self.menuButton, context: context)
+                
+                }, completion: nil)
+            
+        case clearAllButton:
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.customFocus(previouslyFocused: self.menuButton, nextFocused: self.clearAllButton, context: context)
+                
+                }, completion: nil)
+            
+        case saveButton:
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.customFocus(previouslyFocused: self.clearAllButton, nextFocused: self.saveButton, context: context)
+                
+                }, completion: nil)
+            
+        case shareButton:
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.customFocus(previouslyFocused: self.saveButton, nextFocused: self.shareButton, context: context)
+                
+                }, completion: nil)
+        default:
+            return
+        }
+    }
+    
+    func customFocus(previouslyFocused: UIButton, nextFocused: UIButton, context: UIFocusUpdateContext) {
+        
+        nextFocused.layer.shouldRasterize = true
+        nextFocused.layer.shadowColor = UIColor.black.cgColor
+        nextFocused.layer.shadowOpacity = 0.5
+        nextFocused.layer.shadowRadius = 25
+        nextFocused.layer.shadowOffset = CGSize(width: 0, height: 16)
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            nextFocused.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        })
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            previouslyFocused.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+        
+        context.previouslyFocusedView?.layer.shadowOffset = CGSize.zero
+        context.previouslyFocusedView?.layer.shadowColor = UIColor.clear.cgColor
+    }
 }
 
 extension PopUpViewController {

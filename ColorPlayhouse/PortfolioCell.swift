4d31ee340cc3246d33a650ac9300a4bc143d5497
priscilarosa <priscilarosa@MacBook-Pro-de-Priscila.local>
2016-11-28 15:15:04 -0200

    Focus on Portfolio Cell

--- ColorPlayhouse/PortfolioCell.swift
+++ ColorPlayhouse/PortfolioCell.swift
@@ -12,5 +12,43 @@ import UIKit
 class PortfolioCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIRemoteImageView!
+
+    var item:Int!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //imageView.adjustsImageWhenAncestorFocused = true
+        
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        if context.nextFocusedView == self {
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                //self.tintColor = .white
+                self.layer.shadowColor = UIColor.black.cgColor
+                self.layer.shadowOpacity = 0.5
+                self.layer.shadowRadius = 25
+                self.layer.shadowOffset = CGSize(width: 0, height: 16)
+                }, completion: nil)
+            
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            })
+
+        }
+            
+        else if context.previouslyFocusedView == self {
+            coordinator.addCoordinatedAnimations ({ () -> Void in
+                self.layer.shadowOffset = CGSize.zero
+                self.layer.shadowColor = UIColor.clear.cgColor
+                }, completion: nil)
+            
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            })
+            
+        }
+    }
 }

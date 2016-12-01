ff8f85a5ce7c4053e13ddd6b3d1b6f9830a7f56e
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2016-12-01 10:47:08 -0200

    Fixed bug that would take double screen shots when starting a new drawing, returning to main menu controller and then starting a new drawing again. Added functionality for clear all and menu buttons in PopUp VC

--- ColorPlayhouse/DrawingViewController.swift
+++ ColorPlayhouse/DrawingViewController.swift
@@ -121,19 +121,21 @@ class DrawingViewController: UIViewController {
 	@objc private func menuButtonWasPressed() {
 		
 		if popupIsOpen {
-			
 			dismissPopUp()
 			
 			view.gestureRecognizers?.forEach({$0.isEnabled = true})
-			menuButtonTap?.isEnabled = false
 			popupIsOpen = false
-		}
+            timer.fire()
+        } else {
+            timer.invalidate()
+            self.dismiss(animated: true, completion: nil)
+        }
 	}
 	
 	@objc private func playButtonWasPressed() {
 		
 		timer.invalidate()
-		
+
 		view.gestureRecognizers?.forEach({$0.isEnabled = false})
 		menuButtonTap?.isEnabled = true
 		popupIsOpen = true
@@ -422,7 +424,6 @@ class DrawingViewController: UIViewController {
 		
 		menuButtonTap = UITapGestureRecognizer(target: self, action: #selector(menuButtonWasPressed))
 		menuButtonTap?.allowedPressTypes = [NSNumber(value: UIPressType.menu.rawValue)]
-		menuButtonTap?.isEnabled = false
 		self.view.addGestureRecognizer(menuButtonTap!)
 		
 		let playButtonTap = UITapGestureRecognizer(target: self, action: #selector(playButtonWasPressed))
